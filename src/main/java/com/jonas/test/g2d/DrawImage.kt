63bb2483d379afc4290be961e30d281f50c19b19
JonasGao <jonas.gao@voyageone.cn>
2017-06-15 11:27:00 +0800

    add: draw image on center

--- src/main/java/com/jonas/test/g2d/DrawImage.kt
+++ src/main/java/com/jonas/test/g2d/DrawImage.kt
@@ -10,33 +10,46 @@ import javax.imageio.ImageIO
  * Created by jonas on 2017/6/15.
  */
 fun main(args: Array<String>) {
-    val image = ImageIO.read(File("/Users/jonas/Desktop/1.jpeg"))
+    val image = ImageIO.read(File("/Users/jonas/Desktop/1.png"))
     val bufferedWidth = 1200.0
     val bufferedHeight = 1200.0
     val width: Double
     val height: Double
+    val x: Double
+    val y: Double
     when {
         image.width > image.height -> {
             width = bufferedWidth
             height = image.height * (bufferedWidth / image.width)
+            x = 0.0
+            y = Math.abs(bufferedHeight - height) / 2
         }
         image.width < image.height -> {
             width = image.width * (bufferedHeight / image.height)
             height = bufferedHeight
+            x = 0.0
+            y = Math.abs(bufferedWidth - width) / 2
         }
         else -> {
-            val length = if (bufferedHeight > bufferedWidth)
-                bufferedWidth
-            else
-                bufferedHeight
+            val length: Double
+            if (bufferedHeight > bufferedWidth) {
+                length = bufferedWidth
+                x = 0.0
+                y = Math.abs(bufferedHeight - bufferedWidth) / 2
+            } else {
+                length = bufferedHeight
+                x = Math.abs(bufferedHeight - bufferedWidth) / 2
+                y = 0.0
+            }
             width = length
             height = length
         }
     }
 
+
     val bufferedImage = BufferedImage(bufferedWidth.toInt(), bufferedHeight.toInt(), BufferedImage.TYPE_INT_ARGB)
     val g2d = bufferedImage.graphics
-    g2d.drawImage(image, 0, 0, width.toInt(), height.toInt(), null)
+    g2d.drawImage(image, x.toInt(), y.toInt(), width.toInt(), height.toInt(), null)
     g2d.dispose()
     val out: FileOutputStream
     try {
